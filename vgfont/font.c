8c34188319db6481dcceab34184dcbdc0d6f6bd8
Joel Haasnoot <joelhaasnoot@gmail.com>
2016-09-02 10:11:54 +0200

    Add error handling

--- vgfont/font.c
+++ vgfont/font.c
@@ -247,6 +247,9 @@ VCOS_STATUS_T gx_priv_render_text(GX_DISPLAY_T *disp,
 
    vgDestroyPaint(fg);
 
+   if (vgGetError() != 0) {
+        GC_LOG("Error drawing %d", vgGetError)
+   }
    vcos_assert(vgGetError() == 0);
    vgSeti(VG_SCISSORING, VG_FALSE);
 
